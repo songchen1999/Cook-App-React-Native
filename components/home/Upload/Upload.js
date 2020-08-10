f69e5d5304cefa66c1e7f91a5af6682025b0dc8d
Song Chen <ChenSong@s-MacBook-Pro.local>
2020-08-10 00:19:32 -0400

    changed template to ingredients, fixed keyboard problem, trying to solve ingredient data format problem

--- components/home/Upload/Upload.js
+++ components/home/Upload/Upload.js
@@ -10,10 +10,13 @@ import {
   SafeAreaView,
   StyleSheet,
   View,
+  ScrollView,
   Dimensions,
 } from "react-native";
 
-const createQuestion = () => ({
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+const createIngredient = () => ({
   text: "",
 });
 
@@ -32,116 +35,118 @@ export default class Upload extends Component {
           backgroundColor: "black",
         }}
       >
-        <Text style={styles.text}>Upload your recipe</Text>
-        <View style={styles.form}>
-          <Formik
-            initialValues={{
-              uri: "",
-              description: "",
-              text: "",
-              name: "",
-              questions: [],
-            }}
-            onSubmit={(values) => Alert.alert(JSON.stringify(values))}
-            validationSchema={yup.object().shape({
-              uri: yup.string().url().required(),
-              description: yup.string().min(6).required(),
-              text: yup.string().min(6).required(),
-              name: yup.string().min(6).required(),
-            })}
-          >
-            {({
-              values,
-              handleChange,
-              errors,
-              handleBlur,
-              setFieldTouched,
-              setFieldValue,
-              touched,
-              isValid,
-              handleSubmit,
-            }) => (
-              <Fragment>
-                <TextInput
-                  value={values.name}
-                  style={styles.TextInput}
-                  onChangeText={handleChange("name")}
-                  placeholderTextColor="white"
-                  onBlur={() => setFieldTouched("name")}
-                  placeholder="Name of the recipe"
-                />
-                {touched.name && errors.name && (
-                  <Text style={{ fontSize: 10, color: "red" }}>
-                    {errors.name}
-                  </Text>
-                )}
-                <TextInput
-                  value={values.uri}
-                  style={styles.TextInput}
-                  onChangeText={handleChange("uri")}
-                  placeholder="uri"
-                  placeholderTextColor="white"
-                  onBlur={() => setFieldTouched("uri")}
-                />
-                {touched.uri && errors.uri && (
-                  <Text style={{ fontSize: 10, color: "red" }}>
-                    {errors.uri}
-                  </Text>
-                )}
-                <TextInput
-                  value={values.description}
-                  style={styles.TextInput}
-                  placeholderTextColor="white"
-                  onChangeText={handleChange("description")}
-                  placeholder="Description"
-                  onBlur={() => setFieldTouched("description")}
-                />
-                {touched.description && errors.description && (
-                  <Text style={{ fontSize: 10, color: "red" }}>
-                    {errors.description}
-                  </Text>
-                )}
-                <TextInput
-                  value={values.text}
-                  style={styles.TextInput}
-                  placeholderTextColor="white"
-                  onChangeText={handleChange("text")}
-                  placeholder="text"
-                  onBlur={() => setFieldTouched("text")}
-                />
-                {touched.text && errors.text && (
-                  <Text style={{ fontSize: 10, color: "red" }}>
-                    {errors.text}
-                  </Text>
-                )}
-                <Button
-                  title="Submit"
-                  disabled={!isValid}
-                  onPress={handleSubmit}
-                />
-
-                {values.questions.map(({ text }, index) => (
+        <KeyboardAwareScrollView>
+          <Text style={styles.text}>Upload your recipe</Text>
+          <View style={styles.form}>
+            <Formik
+              initialValues={{
+                uri: "",
+                description: "",
+                text: "",
+                name: "",
+                ingredients: [],
+              }}
+              onSubmit={(values) => Alert.alert(JSON.stringify(values))}
+              validationSchema={yup.object().shape({
+                uri: yup.string().url().required(),
+                description: yup.string().min(6).required(),
+                text: yup.string().min(6).required(),
+                name: yup.string().min(6).required(),
+              })}
+            >
+              {({
+                values,
+                handleChange,
+                errors,
+                handleBlur,
+                setFieldTouched,
+                setFieldValue,
+                touched,
+                isValid,
+                handleSubmit,
+              }) => (
+                <Fragment>
                   <TextInput
-                    key={index}
-                    onChangeText={handleChange(`questions[${index}].text`)}
-                    onBlur={handleBlur(`questions[${index}].text`)}
-                    value={values.questions[index].text}
+                    value={values.name}
+                    style={styles.TextInput}
+                    onChangeText={handleChange("name")}
+                    placeholderTextColor="white"
+                    onBlur={() => setFieldTouched("name")}
+                    placeholder="Name of the recipe"
                   />
-                ))}
-                <Button
-                  onPress={() =>
-                    setFieldValue("questions", [
-                      ...values.questions,
-                      createQuestion(),
-                    ])
-                  }
-                  title="Add question"
-                />
-                <Button onPress={handleSubmit} title="Submit" />
-              </Fragment>
-            )}
-          </Formik>
-        </View>
+                  {touched.name && errors.name && (
+                    <Text style={{ fontSize: 10, color: "red" }}>
+                      {errors.name}
+                    </Text>
+                  )}
+                  <TextInput
+                    value={values.uri}
+                    style={styles.TextInput}
+                    onChangeText={handleChange("uri")}
+                    placeholder="uri"
+                    placeholderTextColor="white"
+                    onBlur={() => setFieldTouched("uri")}
+                  />
+                  {touched.uri && errors.uri && (
+                    <Text style={{ fontSize: 10, color: "red" }}>
+                      {errors.uri}
+                    </Text>
+                  )}
+                  <TextInput
+                    value={values.description}
+                    style={styles.TextInput}
+                    placeholderTextColor="white"
+                    onChangeText={handleChange("description")}
+                    placeholder="Description"
+                    onBlur={() => setFieldTouched("description")}
+                  />
+                  {touched.description && errors.description && (
+                    <Text style={{ fontSize: 10, color: "red" }}>
+                      {errors.description}
+                    </Text>
+                  )}
+                  <TextInput
+                    value={values.text}
+                    style={styles.TextInput}
+                    placeholderTextColor="white"
+                    onChangeText={handleChange("text")}
+                    placeholder="text"
+                    onBlur={() => setFieldTouched("text")}
+                  />
+                  {touched.text && errors.text && (
+                    <Text style={{ fontSize: 10, color: "red" }}>
+                      {errors.text}
+                    </Text>
+                  )}
+
+                  {values.ingredients.map(({ text }, index) => (
+                    <TextInput
+                      style={styles.TextInput}
+                      key={index}
+                      onChangeText={handleChange(`ingredients[${index}].text`)}
+                      onBlur={handleBlur(`ingredients[${index}].text`)}
+                      value={values.ingredients[index].text}
+                    />
+                  ))}
+                  <Button
+                    onPress={() =>
+                      setFieldValue("ingredients", [
+                        ...values.ingredients,
+                        createIngredient(),
+                      ])
+                    }
+                    title="Add an ingredient"
+                  />
+                  <Button
+                    title="Submit"
+                    disabled={!isValid}
+                    onPress={handleSubmit}
+                  />
+                </Fragment>
+              )}
+            </Formik>
+          </View>
+        </KeyboardAwareScrollView>
       </SafeAreaView>
     );
   }
@@ -158,7 +163,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   TextInput: {
-    height: "10%",
+    height: 20,
     backgroundColor: "rgb(37,37,38)",
     width: "80%",
     color: "white",
