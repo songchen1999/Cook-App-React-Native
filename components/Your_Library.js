cb5aed0473bd505f0f129d333929205c2e90e004
Song Chen <ChenSong@s-MacBook-Pro.local>
2020-08-11 01:05:08 -0400

    add checklogin method, and now place it to restrict submit option

--- components/Your_Library.js
+++ components/Your_Library.js
@@ -1,8 +1,22 @@
 import * as React from "react";
+import firebase from "firebase";
 import { SafeAreaView, Text, Button } from "react-native";
 
 function Libary({ navigation }) {
-  return <SafeAreaView></SafeAreaView>;
+  const checkifLoggedIn = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log("user logged in");
+      } else {
+        console.log("user not logged in");
+      }
+    });
+  };
+  return (
+    <SafeAreaView>
+      <Button onPress={checkifLoggedIn} title="CheckLogin" />
+    </SafeAreaView>
+  );
 }
 
 export default Libary;
--- components/home/Show/Setting/Setting.js
+++ components/home/Show/Setting/Setting.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
 import { SimpleLineIcons } from "@expo/vector-icons";
+import firebase from "firebase";
+
+const checkifLoggedIn = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log("user logged in");
+      return true;
+    } else {
+      console.log("user not logged in");
+      return false;
+    }
+  });
+};
 
 export default class Setting extends Component {
   render() {
     return (
       <TouchableOpacity
         onPress={() => {
-          this.props.navigation.navigate("Upload");
+          if (checkifLoggedIn()) {
+            this.props.navigation.navigate("Upload");
+          }
         }}
       >
         <View style={styles.Setting}>
