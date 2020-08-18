10c2078e1dd698190936e7f9f7af2d89e433581e
Song Chen <ChenSong@s-MacBook-Pro.local>
2020-08-17 22:08:15 -0400

    trying to figure out how to github CLI

--- App.js
+++ App.js
@@ -2,14 +2,7 @@ import React, { useEffect } from "react";
 import Nav from "./navigation/navigation";
 import * as firebase from "firebase";
 firebase.initializeApp({
-  apiKey: "AIzaSyBBMFPWeZ-Igh84ZlcpPrEo5-fgHRpJ-_A",
-  authDomain: "cook-react-native.firebaseapp.com",
-  databaseURL: "https://cook-react-native.firebaseio.com",
-  projectId: "cook-react-native",
-  storageBucket: "cook-react-native.appspot.com",
-  messagingSenderId: "776274146272",
-  appId: "1:776274146272:web:55ea74c2ed066a0befb185",
-  measurementId: "G-XE25PVDRDM",
+  // please input your own configure that you can find in firebase console
 });
 
 const App = () => {
